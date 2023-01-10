ef1dda7f8ece7b251d5cd67748c85f3cd4967582
Arima057nn <tjendung97pain@gmail.com>
2023-01-10 14:38:37 +0700

    test1

---
+++ src/data/index.js
@@ -0,0 +1,28 @@
+export const books = [
+  {
+    id: 1,
+    name: "Super Backpack",
+    price: 129.99,
+    description:
+      "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+    image: "https://cf.shopee.vn/file/sg-11134201-22100-gl6k8whuk7iva4",
+  },
+  {
+    id: 2,
+    name: "New Hip",
+    price: 199.99,
+    description:
+      "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+    image:
+      "https://salt.tikicdn.com/ts/product/45/3b/fc/aa81d0a534b45706ae1eee1e344e80d9.jpg",
+  },
+  {
+    id: 3,
+    name: "Elite Series",
+    price: 189.99,
+    description:
+      "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
+    image:
+      "https://salt.tikicdn.com/ts/product/45/3b/fc/aa81d0a534b45706ae1eee1e344e80d9.jpg",
+  },
+];
